15533051aed3f2c393499b50a6bcc101dd4234d9
licl <licl@connor.net.cn>
2022-04-23 20:58:53 +0800

    springboot的demo提交--新增笔记

--- springboot_01_hello/src/main/java/com/atguigu/springboot/config/BeanConfig.java
+++ springboot_01_hello/src/main/java/com/atguigu/springboot/config/BeanConfig.java
@@ -1,17 +1,18 @@
 package com.atguigu.springboot.config;
 
 import ch.qos.logback.core.db.DBHelper;
+import com.atguigu.springboot.bean.Car;
 import com.atguigu.springboot.bean.Pet;
 import com.atguigu.springboot.bean.User;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration(proxyBeanMethods = true)
-//@Import({User.class, DBHelper.class})
-
+@EnableConfigurationProperties(Car.class)
 public class BeanConfig {
 
     @Bean("user01")
@@ -25,4 +26,5 @@ public class BeanConfig {
     public Pet pet1(){
         return new Pet("jack");
     }
+
 }
--- springboot_01_hello/src/main/java/com/atguigu/springboot/controller/HelloController.java
+++ springboot_01_hello/src/main/java/com/atguigu/springboot/controller/HelloController.java
@@ -1,13 +1,22 @@
 package com.atguigu.springboot.controller;
 
+import com.atguigu.springboot.bean.Car;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("hello")
+    @Autowired
+    Car car;
+    @RequestMapping("/hello")
     public String hello(){
         return "hello springboot2";
+    }
+
+    @RequestMapping("/car")
+    public Car car(){
+        return car;
     }
 }
